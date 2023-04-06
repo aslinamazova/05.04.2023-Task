f8bd09a944e962bf3076c552b3df64b35fb76705
aslinamazova <76001yw@code.edu.az>
2023-04-06 10:55:02 +0400

    first commit

--- Program.cs
+++ Program.cs
@@ -1,73 +1,19 @@
-﻿#region Task1
-//Print(16);
-//void Print(int num)
-//{
-//    string CheckOddOrEven(int num)
-//    {
-//        if (num % 2 == 0)
-//        {
-//            return "Cutdur";
-//        }
-
-//        return "Tekdir";
-//    }
-//    string result = CheckOddOrEven(num);
-//    Console.WriteLine(result);
-//}
-#endregion
-
-#region Task2
-
-//int a, b;
-//Console.WriteLine("Please enter your number: ");
-//a = int.Parse(Console.ReadLine());
-
-//for (b = 2; a > 1; b++)
-//    if (a % b == 0)
-//    {
-//        while (a % b == 0)
-//        {
-//            a /= b;
-
-//        }
-//        Console.WriteLine($"{b} is a prime factor");
-//    }
-
-#endregion
-
-#region Task3
-
-void Print(double num)
+﻿namespace Task2
 {
-    Console.WriteLine(num);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method(3,2);
+        }
+        static void Method(int a)
+        {
+            Console.WriteLine(Math.Pow(a,a));
+        }
+        static void Method(int a,int b)
+        {
+            Console.WriteLine(Math.Pow(a,b));
+        }
+    }
 }
 
-double num1 = 5;
-double num2 = 6;
-double result = Difference(num1, num2);
-
-
-Print(result);
-double Sum(double num1, double num2)
-{
-    return num1 + num2;
-}
-double Difference(double num1, double num2)
-{
-    return num1 - num2;
-}
-double multiplication (double num1,double num2)
-{
-    return num1 * num2;
-}
-double divided (double num1,double num2)
-{
-    return num1 / num2;
-}
-
-
-
-
-
-
-#endregion
